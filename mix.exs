7760e7c5f7e6394a0088d1118e148dec5a784724
Frank Hunleth <fhunleth@troodon-software.com>
2023-07-07 11:03:30 -0400

    Fix github organization

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi02.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
+  @github_organization "fhunleth"
   @app :nerves_system_rpi0_2
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
